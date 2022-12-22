aed75ff2eb4c1637cf95808bbd29d9b96be37d2b
optesis-als <57010991+optesis-als@users.noreply.github.com>
2022-12-22 12:35:24 +0000

    Delete odoo_create_portal_po_request directory

--- odoo_create_portal_po_request/controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding : utf-8 -*-
-
-from . import vendor_rfq
-from . import portal
--- odoo_create_portal_po_request/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding : utf-8 -*-
-
-from . import purchase_order
-from . import rfq_purchase_requests
